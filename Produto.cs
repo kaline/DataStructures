1a52dbe177c5a1fb09e1fe3f91f680f708ee4c44
kaline <mesquitabfkaline@gmail.com>
2019-07-24 19:48:55 -0300

    Create Produto.cs

---
+++ Produto.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int a, b, Prod;
+		a = Convert.ToInt16(Console.ReadLine());
+		b = Convert.ToInt16(Console.ReadLine());
+		Prod = a*b;
+		Console.WriteLine("PROD = {0}", +Prod);
+
+    }
+
+}
