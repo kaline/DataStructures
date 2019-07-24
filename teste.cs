fc908c01369e8b441b676e8095afbc8f0baa09bf
kaline <mesquitabfkaline@gmail.com>
2019-07-24 19:43:12 -0300

    Update teste.cs

--- teste.cs
+++ teste.cs
@@ -1,36 +1,15 @@
-using System;
+using System; 
 
-namespace RectangleApplication {
-   
-   class Rectangle {
-      
-      double length;
-      double width;
-      
-      public void Acceptdetails() {
-         length = 4.5;    
-         width = 3.5;
-      }
-      
-      public double GetArea() {
-         return length * width; 
-      }
-      
-      public void Display() {
-         Console.WriteLine("Length: {0}", length);
-         Console.WriteLine("Width: {0}", width);
-         Console.WriteLine("Area: {0}", GetArea());
-      }
-      
-   }
-   
-   class ExecuteRectangle {
-   
-      static void Main(string[] args) {
-         Rectangle r = new Rectangle();
-         r.Acceptdetails();
-         r.Display();
-         Console.ReadLine(); 
-      }
-   }
+class URI {
+
+    static void Main(string[] args) { 
+
+        int A, B, X;
+		A = Convert.ToInt32(Console.ReadLine());
+		B = Convert.ToInt32(Console.ReadLine());
+		X = A+B;
+
+		Console.WriteLine("X = {0}",  +X);
+    }
+
 }
