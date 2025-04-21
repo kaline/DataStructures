fa5e4888b9a6bcd4b37b27417cb9879360ec69f0
kaline mesqita <mesquitabfkaline@gmail.com>
2025-04-21 18:18:58 -0300

    remove dir and add dir linkedlist

---
+++ LinkedList/SinglyLinkedList.js
@@ -0,0 +1,21 @@
+
+class Node{
+    constructor(val){
+        this.val = val
+        this.next = null
+        
+    }
+}
+
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+
+a.next = b
+b.next = c
+c.next = d
+
+// a -> b -> c -> d -> null
+
+
